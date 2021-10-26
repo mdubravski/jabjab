43609c54dbdaf8e4c55856dd8c83abff8375aef8
mdubravski <mdubravski@gmail.com>
2021-10-25 23:31:19 -0400

    added code examples

---
+++ primitive-types/app.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * all types = String, Number, Boolean, Object, Null, Undefined, Symbol
+ * 
+ */
+
+
+// primitive types
+let a = "Michael"; // String
+let b = 10; // Number
+let c = true; // Boolean
