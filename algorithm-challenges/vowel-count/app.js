16f19cd9e22a7e65afdbad04270fafe515cc97c7
mdubravski <mdubravski@gmail.com>
2021-10-26 13:54:31 -0400

    refactor vowel-count

--- algorithm-challenges/vowel-count/app.js
+++ algorithm-challenges/vowel-count/app.js
@@ -1,37 +1,60 @@
 /**
  * 
- *  count the number of vowels in a given string
+ *  different functions for counting the number of vowels in a given string
  * 
  */
 
- let s = "Hi how are you today?";
- s = s.toLowerCase();
- arr = s.split("");
- letters = s.split("");
- var count = 0;
- 
- for(let i = 0; i < letters.length; i++){
-    
-     switch(letters[i]){
-        case 'a':
+function countVowels1(str){
+    str=str.toLowerCase();
+    const vowels = "aeiou";
+    let count = 0;
+    for(let i=0; i<str.length;i++){
+        if(vowels.includes(str[i])){
             count++;
-            break;
-        case 'e':
-            count++;
-            break;
-            case 'i':
-            count++;
-            break;
-        case 'o':
-            count++;
-            break;
-        case 'u':
-            count++;
-            break;
-        default:
-            break;
-     }
+        }
+    }
+    return count;
+}
 
- }
+function countVowels2(str){
+    str = str.toLowerCase();
+    const vowels = "aeiou";
+    let count = 0;
+    for(let s of str){
+        if(vowels.includes(s)){
+            count++;
+        }
+    }
+    return count;
+}
 
-console.log('# of vowels:',count);
+function countVowels3(str){
+    let count = 0;
+    for(let s of str){
+        switch(s){
+            case "a":
+                count++;
+                break;
+            case "e":
+                count++;
+                break;
+            case "i":
+                count++;
+                break;
+            case "o":
+                count++;
+                break;
+            case "u":
+                count++;
+                break;
+            default:
+                break;
+         }
+    }
+    return count;
+}
+
+let s = "Hi how are you?";
+console.log(countVowels1(s));
+console.log(countVowels2(s));
+console.log(countVowels3(s));
