05541aec10a4cb50ec5d1af22ed3f4c88d5deeb2
mdubravski <mdubravski@gmail.com>
2021-10-31 15:02:19 -0400

    add null-undefiend examples

---
+++ adv-DOM/null-undefined/app.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * undefined = variable where value is not assigned
+ * 
+ * null = represents a non-existant value
+ * 
+ */
+
+// declared varaible that is not yet assigned
+let age;
+console.log(age);
+
+let food = null;
+console.log(food);
