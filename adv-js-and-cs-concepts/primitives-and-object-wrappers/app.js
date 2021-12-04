55de51e0166b152d363e5e6153e632e358e0ced5
mdubravski <mdubravski@gmail.com>
2021-12-04 12:12:58 -0500

    add primitives and obj wrappers example

---
+++ adv-js-and-cs-concepts/primitives-and-object-wrappers/app.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Primitive
+ *  - data that is not an obj and has no methods
+ *  - primitive values (except null and undefined) have obj wrappers that allow for use of functions
+ *  - string, number, boolean, symbol, undefined, null
+ * 
+ * Reference (Object)
+ * 
+ */
+
+console.log(typeof 'hello'); // same
+console.log(typeof new String('hello')); // same
