352d0e29735e93095dc43a4e5d1ea09ee5b0e31e
mdubravski <mdubravski@gmail.com>
2021-12-28 15:11:21 -0500

    add async callbacks example

---
+++ async/async-callbacks/app.js
@@ -0,0 +1,43 @@
+// call backs
+const boilWater = (callback) => {
+    setTimeout(_ => {
+        callback('water ready.');
+    }, 10000);
+};
+
+const prepareOnions = (callback) => {
+    setTimeout(_ => {
+        callback('onions ready.');
+    }, 3000);
+};
+
+const prepareEggs = (callback) => {
+    setTimeout(_ => {
+        callback('eggs ready.');
+    }, 2000);
+};
+
+const mix = (callback) => {
+    setTimeout(_ => {
+        callback('mixed and ready to eat.');
+    }, 2000);
+};
+
+function eatRamen(){
+    boilWater((elem) => {
+        console.log(elem);
+        prepareOnions(elem => {
+            console.log(elem);
+            prepareEggs(elem => {
+                console.log(elem);
+                mix(elem => {
+                    console.log(elem);
+                });
+            });
+        });
+    });
+}
+
+eatRamen();
+
+
